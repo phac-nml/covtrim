342da5c41dd343507857c0f471c16ab5f5544bb1
Gurasis Osahan <gurasis.osahan@phac-aspc.gc.ca>
2024-10-28 14:05:52 -0500

    Updated setup files to map Python script.

--- setup.py
+++ setup.py
@@ -2,68 +2,75 @@
 import setuptools
 
 # Project metadata
-name = "covtrim"
-version = "0.1.0"
-author = "Gurasis Osahan"
-author_email = "gurasis.osahan@phac-aspc.gc.ca"
-description = "CovTrim is a specialized bioinformatics tool designed for precise coverage-based downsampling of FASTQ files from amplicon sequencing data."
-long_description = """
-CovTrim is a specialized bioinformatics tool designed for precise coverage-based downsampling of FASTQ files from amplicon sequencing data. It intelligently adjusts sequencing depth while maintaining quality metrics and amplicon representation, making it particularly useful for viral genomics, amplicon-based sequencing projects, and high-throughput sequencing optimization.
+NAME = "covtrim"
+VERSION = "0.1.0"
+AUTHOR = "Gurasis Osahan"
+AUTHOR_EMAIL = "gurasis.osahan@phac-aspc.gc.ca"
+DESCRIPTION = "CovTrim is a specialized bioinformatics tool designed for precise coverage-based downsampling of FASTQ files from amplicon sequencing data."
+LONG_DESCRIPTION = """
+CovTrim is a specialized bioinformatics tool designed for precise coverage-based downsampling of FASTQ files from amplicon sequencing data. 
+It intelligently adjusts sequencing depth while maintaining quality metrics and amplicon representation, making it particularly useful for 
+viral genomics, amplicon-based sequencing projects, and high-throughput sequencing optimization.
 """
-url = "https://github.com/phac-nml/covtrim"
-license_type = "Apache License, Version 2.0"
+URL = "https://github.com/phac-nml/covtrim"
+LICENSE = "Apache License, Version 2.0"
 
 # Define project dependencies
-dependencies = [
-    'wheel',
-    "pip",
-    "build",
+DEPENDENCIES = [
     "pysam",
     "numpy",
     "scipy",
     "pandas",
 ]
 
-entry_points = {"console_scripts": ["covtrim=covtrim.covtrim:main"]}
-
-# Project classifiers (https://pypi.org/classifiers/)
-classifiers = [
-    'License :: OSI Approved :: Apache Software License',
-    'Environment :: Console',
-    'Programming Language :: Python :: 3',
-]
-
-# Additional project information
-keywords = "Bioinformatics genomics NGS pipeline"
-project_urls = {
-    "Documentation": "https://github.com/phac-nml/covtrim/raw/branch/main/README.md",
-    "Source Code": "https://github.com/phac-nml/covtrim",
-    "Issue Tracker": "https://github.com/phac-nml/covtrim/issues",
-    "Examples": "https://github.com/phac-nml/covtrim/examples",
-    "FAQ": "https://github.com/phac-nml/covtrim/wiki/FAQ",
-    "License": "https://github.com/phac-nml/covtrim/raw/branch/main/LICENSE"
+ENTRY_POINTS = {
+    "console_scripts": [
+        "covtrim=covtrim.covtrim:main"
+    ]
 }
 
-# Package configuration
+# Project classifiers
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: Apache Software License',
+    'Environment :: Console',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+]
+
+PROJECT_URLS = {
+    "Documentation": "https://github.com/phac-nml/covtrim/blob/main/README.md",
+    "Source Code": "https://github.com/phac-nml/covtrim",
+    "Issue Tracker": "https://github.com/phac-nml/covtrim/issues",
+    "Examples": "https://github.com/phac-nml/covtrim/tree/main/examples",
+    "FAQ": "https://github.com/phac-nml/covtrim/wiki/FAQ",
+    "License": "https://github.com/phac-nml/covtrim/blob/main/LICENSE"
+}
+
 setuptools.setup(
-    name=name,
-    version=version,
-    author=author,
-    author_email=author_email,
-    description=description,
-    long_description=long_description,
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url=url,
-    license=license_type,
-    install_requires=dependencies,
-    entry_points=entry_points,
-    classifiers=classifiers,
-    python_requires='<=3.11',
-    keywords=keywords,
-    project_urls=project_urls,
-    packages=setuptools.find_namespace_packages(where='src'),
-    package_dir={"": "src"},
+    url=URL,
+    license=LICENSE,
+    install_requires=DEPENDENCIES,
+    entry_points=ENTRY_POINTS,
+    classifiers=CLASSIFIERS,
+    python_requires='>=3.8,<=3.11',
+    keywords="bioinformatics genomics NGS pipeline coverage trimming FASTQ",
+    project_urls=PROJECT_URLS,
+    packages=setuptools.find_packages(),  # Changed from find_namespace_packages
     include_package_data=True,
-    platforms='any',
+    platforms='linux',
     zip_safe=False,
 )
