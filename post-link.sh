a9ed09369c884da8eb0ea9231fffdd9ffb68359f
Gurasis Osahan <gurasis.osahan@phac-aspc.gc.ca>
2024-10-28 11:41:17 -0500

    Added install of Samtools to post-link.sh

--- post-link.sh
+++ post-link.sh
@@ -2,6 +2,16 @@
 
 set +e  
 
+# Second installation block with conda
+"${PREFIX}/bin/conda" install -c defaults -c conda-forge -c bioconda -c gosahan "samtools>=1.21" --yes
+
+# Capture the exit code of the conda installation
+conda_install_exit_code=$?
+
+if [ $conda_install_exit_code -ne 0 ]; then
+    echo "conda installation command exited with error code $conda_install_exit_code, but continuing script execution."
+fi
+
 # Python package installations
 declare -a packages=(
     "setuptools"
